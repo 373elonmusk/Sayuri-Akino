02a3488ebc13d7608c2aa217b361dc1837ebcc87
373elonmusk <concise-until-flop@duck.com>
2026-05-20 12:09:39 +0530

    Add files via upload

--- plugins/route.py
+++ plugins/route.py
@@ -1,12 +1,10 @@
 from aiohttp import web
-from .route import routes
 
+routes = web.RouteTableDef()
 
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
-
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Madflix_Bots")
 
 
 
